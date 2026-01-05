40d61c567f659b6244826c3678c202f0f79bae85
RamisaAnanOrni <ramisa.anan.orni@g.bracu.ac.bd>
2026-01-05 22:11:04 +0600

    Feat: Completed the Testimonial secttion

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,8 @@ import Hero from './Hero'
 import WhyChoose from './WhyChoose'
 import Products from '../shop/Products'
 import Experiences from './Experiences'
+import Materials from './Materials'
+import Testimonials from './Testimonials'
 
 const Home = () => {
   return (
@@ -11,6 +13,8 @@ const Home = () => {
     <WhyChoose/>
     <Products headline="Best Selling products"/>
     <Experiences/>
+    <Materials/>
+    <Testimonials/>
     </>
   )
 }
